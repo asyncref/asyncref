a15172d27671d48f6509fb0f25da60d83a452d5b
TomasDurica <tomas.durica.sk@gmail.com>
2023-03-27 22:47:35 +0200

    Added fromRefs tests

--- packages/vue/fromRefs/index.test.ts
+++ packages/vue/fromRefs/index.test.ts
@@ -1,3 +1,43 @@
-describe.todo('fromPromise', () => {
-  // todo
+import { fromRefs } from '@asyncref/vue'
+import { expectLoading, expectRejected, expectResolved } from '@vue-utilities/testUtilities'
+
+describe('fromRefs', () => {
+  describe('loadingState', () => {
+    it('should be created when isLoading is true', () => {
+      const refs = createRefs(true, false, 'resolved', 'rejected')
+
+      const ref = fromRefs(refs)
+
+      expectLoading(ref)
+    })
+  })
+
+  describe('resolvedState', () => {
+    it('should be created when isLoading is false and isError is false', () => {
+      const refs = createRefs(false, false, 'resolved', 'rejected')
+
+      const ref = fromRefs(refs)
+
+      expectResolved(ref).with('resolved')
+    })
+  })
+
+  describe('rejectedState', () => {
+    it('should be created when isLoading is false and isError is true', () => {
+      const refs = createRefs(false, true, 'resolved', 'rejected')
+
+      const ref = fromRefs(refs)
+
+      expectRejected(ref).with('rejected')
+    })
+  })
 })
+
+const createRefs = <TData, TError = string>(isLoading: boolean, isError: boolean, data: TData, error: TError) => {
+  return {
+    isLoading: ref(isLoading),
+    isError: ref(isError),
+    data: ref(data),
+    error: ref(error)
+  }
+}
--- packages/vue/index.ts
+++ packages/vue/index.ts
@@ -1,5 +1,7 @@
 export type { AsyncRef, UnwrapState, UnwrapData, UnwrapError } from './asyncRef'
 
+export { loadingState, resolvedState, rejectedState, isLoading, isResolved, isRejected } from '@asyncref/core'
+
 export { asyncRef } from './asyncRef'
 export { compose } from './compose'
 export { fromPromise } from './fromPromise'
